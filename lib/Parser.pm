207e51a715b319c5bfdfd172bca9fcede2063bf9
ryotarogoto <fifty.fiftyryo0401.0613@gmail.com>
2014-01-19 22:58:20 +0900

    copy ltsv.log

--- lib/Parser.pm
+++ lib/Parser.pm
@@ -3,3 +3,12 @@ package Parser;
 use strict;
 use warnings;
 
+sub new {
+    my ($class, %args) = @_;
+    my $self = {%args};
+    bless $self, $class;
+}
+
+sub parse {
+    open my $fh, '<', $self->{filename} or die $!;
+    my @lines = <$fh>;
